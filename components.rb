8e74a3d793844fd687b0ad23142f7e1e25c3d9c2
Camilo Monsalve <cmonsalsilva@gmail.com>
2020-08-28 16:03:24 -0400

    final?

--- components.rb
+++ components.rb
@@ -7,30 +7,37 @@ key = '5WD4J4YMTZHPm4F48IPtd2EX9bqOfetqri2uvS5L'
 def buid_web_page(hash)
 
      index = '
+          <!DOCTYPE html>
+          <html lang="es">
 
-          <html>
           <head> 
-          <h1 style="background-color:#993300; display:flex; height:150px; justify-content:center;align-items:center; font-size:2rem;"> NASA OFFICIAL SITE </h1>
+               <meta charset="UTF-8">
+               <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+               <h1 style="background-image:url(https://chematierra.mx/files/5814/8650/5647/Marte.png);background-size:cover;background-repeat:round;height:400px; display:flex; justify-content:center;align-items:center; font-size:2rem;"> NASA OFFICIAL SITE </h1>
+
+               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
           </head>
 
-          <body> <h3 style="text-align:center; margin-bottom:5rem;margin-top:3rem;"> CURIOSITY PHOTOS </h3>
-          <ul> '
+          <body> 
 
-          hash["photos"].each do |imagen|
-               index += "<li style='text-align:center;background-color:khaki'>
-               <img src = '#{imagen["img_src"]}'></li>"
-          end    
+               <h3 style="text-align:center; margin-bottom:unset;margin-top:1rem;"> CURIOSITY PHOTOS </h3>
+               
+               <ul> '
 
-     index += "
+                    hash["photos"].each do |imagen|
+                         index += "<li style='text-align:center;background-color:khaki;padding:50px;'>
+                         <img src = '#{imagen["img_src"]}'></li>"
+                    end    
 
-     </ul>
-     <footer> 
-          <p style='background-color:#993300; display:flex; align-items:center;justify-content:center; height:200px; font-size:2rem'> NASA API'S OFFICIAL SITE</p> 
-     </footer>
+          index += "
+
+               </ul>
+          <footer> 
+               <p style='background-image:url(https://www.rockandpop.cl/wp-content/uploads/2018/09/planeta-marte.jpg);background-size:cover;background-repeat:round; display:flex; align-items:center;justify-content:center; height:300px; font-size:2rem'> NASA API'S OFFICIAL SITE</p> 
+          </footer>
      
-     </body>
+          </body>
      </html>"
     
 end     
