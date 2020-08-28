a1317512d16fc8d803f4ce845ea0b92b8fea78d5
Camilo Monsalve <cmonsalsilva@gmail.com>
2020-08-28 14:51:30 -0400

    styles

--- components.rb
+++ components.rb
@@ -10,25 +10,26 @@ def buid_web_page(hash)
 
           <html>
           <head> 
-          <h1 style="background-color:brown; display-flex:center;"> NASA OFFICIAL SITE </h1>
+          <h1 style="background-color:#993300; display:flex; height:150px; justify-content:center;align-items:center; font-size:2rem;"> NASA OFFICIAL SITE </h1>
+
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
           </head>
-          <body> <h3 class="align-text:center;"> CURIOSITY PHOTOS </h3>
+
+          <body> <h3 style="text-align:center; margin-bottom:5rem;margin-top:3rem;"> CURIOSITY PHOTOS </h3>
           <ul> '
 
           hash["photos"].each do |imagen|
-               index += "<li><img src = '#{imagen["img_src"]}'></li>"
+               index += "<li style='text-align:center;background-color:khaki'>
+               <img src = '#{imagen["img_src"]}'></li>"
           end    
 
      index += "
 
      </ul>
      <footer> 
-          <p style='background-color:brown; display-flex:center; align-items:center;justify-content:center'> NASA API'S OFFICIAL SITE</p> 
+          <p style='background-color:#993300; display:flex; align-items:center;justify-content:center; height:200px; font-size:2rem'> NASA API'S OFFICIAL SITE</p> 
      </footer>
-          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+     
      </body>
      </html>"
     
